8d2c35740d48e0c87c8ea2d74fb084aaf0efb7d7
David Tran <david.l.tran@sjsu.edu>
2018-09-16 23:40:07 -0700

    Update #3
    Overhaul all of the images and events for Mid-Pac 2019 (includes note about Steel Bridge/Mystery Competition).

--- src/pages/events.js
+++ src/pages/events.js
@@ -7,6 +7,9 @@ import BannerLanding from '../components/BannerLanding'
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
+import pic12 from '../assets/images/pic12.jpg'
+import pic13 from '../assets/images/pic13.jpg'
+import pic14 from '../assets/images/pic14.png'
 
 const Landing = (props) => (
     <Layout>
@@ -23,12 +26,12 @@ const Landing = (props) => (
                     <header className="major">
                         <h2>Competitions</h2>
                     </header>
-                    <p>Read about our various competitve events down below!</p>
+                    <p>Read about our various competitve events down below! If available, click on the images for each event to see the rules.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
                 <section>
-                    <Link to="/generic" className="image">
+                    <Link to="/404" className="image">
                         <img src={pic08} alt="" />
                     </Link>
                     <div className="content">
@@ -37,15 +40,12 @@ const Landing = (props) => (
                                 <h3>Steel Bridge</h3>
                             </header>
                             <p>Students design, fabricate, erect, and test a steel bridge that meets specifications
-                                and optimizes performance and economy.<br></br>This event will not be hosted for the 2019 competition until further notice.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
+                                and optimizes performance and economy.<br></br>Note: This event will not be hosted for the 2019 competition until further notice.</p>
                         </div>
                     </div>
                 </section>
                 <section>
-                    <Link to="/generic" className="image">
+                    <Link to="/404" className="image">
                         <img src={pic09} alt="" />
                     </Link>
                     <div className="content">
@@ -56,14 +56,14 @@ const Landing = (props) => (
                             <p>Students design, construct, and race a canoe made out of concrete. Arguably the most competitve civil engineering competition
                                 in the world, schools fight to keep their national championship hopes from sinking.
                             </p>
-                            <ul className="actions">
+                            {/*<ul className="actions">
                                 <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
+                            </ul>*/}
                         </div>
                     </div>
                 </section>
                 <section>
-                    <Link to="/generic" className="image">
+                    <Link to="/404" className="image">
                         <img src={pic10} alt="" />
                     </Link>
                     <div className="content">
@@ -74,9 +74,62 @@ const Landing = (props) => (
                             <p>Students design a water filter to be construct on site, and test the efficiency of the assembled filter by treating contaminated water.
                                 In this competition, teams are scored based on a design paper, construction efficiency, water test results, and a presentation.
                             </p>
-                            <ul className="actions">
+                            {/*<ul className="actions">
                                 <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
+                        </ul>*/}
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/404" className="image">
+                        <img src={pic12} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Geo-Challenge</h3>
+                            </header>
+                            <p>Students design, present, construct, and test to failure a Mechanically Stabilized Earthwall (MSE) using strips of paper. 
+                                For this challenge, engineering teams are judged and scored based on their presentation poster and the efficiency and quality of their reinforcement design.
+                            </p>
+                            {/*<ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            </ul>*/}
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/404" className="image">
+                        <img src={pic13} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Water Treatment</h3>
+                            </header>
+                            <p>Students design a water filter to be construct on site, and test the efficiency of the assembled filter by treating contaminated water.
+                                In this competition, teams are scored based on a design paper, construction efficiency, water test results, and a presentation.
+                            </p>
+                            {/*<ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                        </ul>*/}
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/404" className="image">
+                        <img src={pic14} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Kiewit Construction Mystery Design Competition</h3>
+                            </header>
+                            <p>The details for this event will be posted. Stay tuned!
+                            </p>
+                            {/*<ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            </ul>*/}
                         </div>
                     </div>
                 </section>
