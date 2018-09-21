d4e20496af3e7d47256681b9da8050f36cbe82bb
davidtranasianman <dtran0419@gmail.com>
2018-09-20 17:43:44 -0700

    Update #7
    Remove copy symbol (temp)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ const Footer = (props) => (
                 <li><a href="https://www.facebook.com/2019-ASCE-Mid-Pacific-Student-Conference-448518282236515/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                {/*<li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>*/}
             </ul>
             <ul className="copyright">
-                <li>&copy; Untitled</li><li>Created by David Tran</li>
+                {/*<li>&copy; Untitled</li>*/}<li>Created by David Tran</li>
             </ul>
         </div>
     </footer>
