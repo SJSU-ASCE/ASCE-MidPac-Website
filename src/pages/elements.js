43d9f56b0b20b74cd6f1bc5db1702f55b2a69739
David Tran <david.l.tran@sjsu.edu>
2018-10-20 21:57:17 -0700

    Update #7d
    Fix root of deploy issue.

--- src/pages/elements.js
+++ src/pages/elements.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 
 import pic03 from '../assets/images/pic03.jpg'
 import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
+import pic09 from '../assets/images/pic09.png'
 import pic10 from '../assets/images/pic10.jpg'
 
 const Elements = (props) => (
