9e34459d90cdd4adb1d1ed86069a1bc7ff9a98ba
David Tran <david.l.tran@sjsu.edu>
2018-11-16 03:16:00 -0800

    Update #13
    Added Geo-Wall rules and rubrics to events page.
    Added donation link and sponsorship PDF to sponsor page.

--- src/pages/events.js
+++ src/pages/events.js
@@ -42,7 +42,7 @@ const Events = (props) => (
                             </header>
                             <p>Students design and build a temporary shelter for a displaced dog during the aftermath of a disaster or emergency.</p>
                             <ul className="actions">
-                                <li><a href="https://www.asce.org/student_conferences" className="button">Learn more</a></li>
+                                <li><a href="https://www.asce.org/student_conferences" className="button">Rules</a></li>
                             </ul>
                         </div>
                     </div>
@@ -60,7 +60,7 @@ const Events = (props) => (
                                 in the world, schools fight to keep their national championship hopes from sinking.
                             </p>
                             <ul className="actions">
-                                <li><a href="https://www.asce.org/rules-and-regulations" className="button">Learn more</a></li>
+                                <li><a href="https://www.asce.org/rules-and-regulations" className="button">Rules</a></li>
                             </ul>
                         </div>
                     </div>
@@ -78,7 +78,7 @@ const Events = (props) => (
                                 In this competition, teams are scored based on a design paper, construction efficiency, water test results, and a presentation.
                             </p>
                             <ul className="actions">
-                                <li><a href="https://docs.google.com/document/d/1LZ0rzwGhEQv8lbC5Roy-uEMCPf5F96swR1tpNGYxLEM/edit?usp=sharing" className="button">Learn more</a></li>
+                                <li><a href="https://docs.google.com/document/d/1LZ0rzwGhEQv8lbC5Roy-uEMCPf5F96swR1tpNGYxLEM/edit?usp=sharing" className="button">Rules</a></li>
                             </ul>
                         </div>
                     </div>
@@ -95,8 +95,11 @@ const Events = (props) => (
                             <p>Students design, present, construct, and test to failure a Mechanically Stabilized Earthwall (MSE) using strips of paper. 
                                 For this challenge, engineering teams are judged and scored based on their presentation poster and the efficiency and quality of their reinforcement design.
                             </p>
+                            <p>The design poster rules can be found<a href="https://docs.google.com/document/d/1pjQfndIagWEXzNF2oE-K9MQ4BF_tI592t9EDEh4F0lQ/edit?usp=sharing">here.</a></p>
+                            <p>The competition appendices, which includes the material specifications, judging/scoring rubrics, and safety appendix, can be found 
+                                <a href="https://docs.google.com/document/d/19fMcwYqjWD7UeKV81E2UFnhJZljvYDCbbf36Du6hjkE/edit?usp=sharing">here.</a></p> 
                             <ul className="actions">
-                                <li><a href="https://docs.google.com/document/d/1Zo8dATSO1rFnTchxC-mvoV3qi94aCgI5txmEIpApAbs/edit?usp=sharing" className="button">Learn more</a></li>
+                                <li><a href="https://docs.google.com/document/d/1Zo8dATSO1rFnTchxC-mvoV3qi94aCgI5txmEIpApAbs/edit?usp=sharing" className="button">Rules</a></li>
                             </ul>
                         </div>
                     </div>
@@ -113,7 +116,7 @@ const Events = (props) => (
                             <p>Student design and present solutions related to transportation and traffic that help improve mobility and safety for transportation systems and help build smarter and better communities.
                             San Jose's city information can be found <a href="https://docs.google.com/document/d/1nqYp06eOrVJVOOeIEF-_CMhMiYqwr2EIB42dKZ6Ei2M/edit?usp=sharing">here.</a></p>
                             <ul className="actions">
-                                <li><a href="https://docs.google.com/document/d/1aankYKlA84DO1tEE0adr1U1VjdgOAQILSLxuZStTeNw/edit?usp=sharing" className="button">Learn more</a></li>
+                                <li><a href="https://docs.google.com/document/d/1aankYKlA84DO1tEE0adr1U1VjdgOAQILSLxuZStTeNw/edit?usp=sharing" className="button">Rules</a></li>
                             </ul>
                         </div>
                     </div>
